cae0a8886d077f437b059e7096017f97c81d5b0d
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-07-22 01:24:04 +0530

    Time: 0 ms (100%), Space: 30.5 MB (12.44%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,9 +5,11 @@ public:
         vector<int> temp(nums.size());
 
         for(int i = 0; i < nums.size(); i++){
-             temp[(i + k)%nums.size()] = nums[i];
 
+            temp[ (i + k) % nums.size() ] = nums[i];
         }
-         nums = temp;
+
+        nums = temp;
     }
+
 };
