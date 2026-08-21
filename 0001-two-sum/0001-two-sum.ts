348b0d9e04bcf1a898933c6c690b07a3f74b2085
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-21 18:28:04 +0530

    Time: 49 ms (15.06%), Space: 56.4 MB (73.07%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+        const n = nums.length;
+
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] === target) {
+                    return [i, j];
+                }
+            }
+        }
+
+       return [];
+}
